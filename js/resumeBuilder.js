54d7ac78dfc37a8f6b347a6a16e309b07681e061
maycatk <tlovecatk@gmail.com>
2015-10-09 07:33:03 +0800

    Revert "前一天课程测试"
    This reverts commit d5337fa213aad31e81d3de1c1cc9ad9625421e71.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,20 +1,3 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-/*$("#main").append(["左璇"]);
-var awesomeThoughts = "I am zuoxuan and I am AWESOME!"
-console.log(awesomeThoughts);
-*/
-/*var email = "cameron@udacity.com";
-var newEmail = email.replace("udacity","gmail");
-console.log(email);
-console.log(newEmail);*/
-/*var awesomeThoughts = "I am zuoxuan and I am AWESOME!"
-var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
-console.log(funThoughts);
-$("#main").append(funThoughts);
-*/
-var formattedName = HTMLheaderName.replace("%data%","zouxuan");
-var formattedRole = HTMLheaderRole.replace("%data%","Web developer");
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
